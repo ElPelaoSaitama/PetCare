697f7958a044f66643e6fe107062e3dd1ea47fab
Gustavo <gustavocartes05@gmail.com>
2023-04-11 01:03:41 -0400

    estructura base

--- PetCare/settings.py
+++ PetCare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Modulos.app-clinica.urls',
+    'Modulos.app_clinica.urls',
+    'Modulos.app_petstore.urls2',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
