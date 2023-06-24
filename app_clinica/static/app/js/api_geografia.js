eadcbd024e3c2862e0df6522d1bd843c5dc1949e
Gustavo Cartes <gustavocartes05@gmail.com>
2023-06-24 04:41:26 -0400

    eddit

--- app_clinica/static/app/js/api_geografia.js
+++ app_clinica/static/app/js/api_geografia.js
@@ -1,38 +1,74 @@
-// Obtener el elemento select
-const selectComunas = document.getElementById('comunas');
+$(document).ready(function() {
+  const regionesUrl = 'http://petcare.ddns.net/api-geografia/api/regiones/';
 
-// Función para crear las opciones del select
-function createComunaOption(comuna) {
-  const option = document.createElement('option');
-  option.value = comuna.id;
-  option.textContent = comuna.nombre;
-  return option;
-}
+  // Obtener regiones mediante AJAX
+  $.ajax({
+    url: regionesUrl,
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      const $regionSelect = $('#region');
+      $regionSelect.empty(); // Limpiar opciones existentes
 
-// Función para cargar las comunas desde la API
-function loadComunas() {
-  fetch('{% url 'app_geografia:comuna-list' %}')
-    .then(response => response.json())
-    .then(data => {
-      // Limpiar opciones existentes
-      selectComunas.innerHTML = '';
+      // Agregar opción de placeholder
+      const placeholderOption = $('<option>').val('').text('Seleccione región');
+      $regionSelect.append(placeholderOption);
 
-      // Crear opción por defecto
-      const defaultOption = document.createElement('option');
-      defaultOption.value = '';
-      defaultOption.textContent = 'Seleccione una comuna';
-      selectComunas.appendChild(defaultOption);
-
-      // Crear opciones para cada comuna
-      data.forEach(comuna => {
-        const option = createComunaOption(comuna);
-        selectComunas.appendChild(option);
+      // Agregar opciones de regiones al select
+      data.forEach(function(region) {
+        const option = $('<option>').val(region.id).text(region.nombre);
+        $regionSelect.append(option);
       });
-    })
-    .catch(error => {
-      console.log('Error:', error);
+    },
+    error: function(xhr, textStatus, errorThrown) {
+      console.log('Error al obtener regiones:', errorThrown);
+    }
+  });
+});
+
+// Obtener referencias a los elementos <select>
+var $regionSelect = $('#region');
+var $comunasSelect = $('#comunas');
+
+// Función para cargar las comunas basadas en la región seleccionada
+function cargarComunas() {
+  var regionId = $regionSelect.val();
+
+  // Verificar si se ha seleccionado una región
+  if (regionId) {
+    // Realizar una solicitud AJAX a la API para obtener las comunas de la región seleccionada
+    // Aquí debes reemplazar la URL con la ruta correcta a tu vista de API de comunas
+    var url = '/api-geografia/api/comunas/?region_id=' + regionId;
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        // Limpiar el <select> de comunas
+        $comunasSelect.empty();
+
+        // Agregar opción de placeholder
+        const placeholderOption = $('<option>').val('').text('Seleccione comuna');
+        $comunasSelect.append(placeholderOption);
+
+        // Agregar las opciones de comunas al <select>
+        data.forEach(function(comuna) {
+          var option = $('<option>').val(comuna.id).text(comuna.nombre);
+          $comunasSelect.append(option);
+        });
+      },
+      error: function(xhr, textStatus, errorThrown) {
+        console.log('Error al cargar las comunas:', errorThrown);
+      }
     });
+  } else {
+    // Si no se ha seleccionado una región, limpiar el <select> de comunas
+    $comunasSelect.empty();
+  }
 }
 
-// Cargar las comunas al cargar la página
-loadComunas();
+// Asociar el evento "change" al <select> de regiones
+$regionSelect.on('change', cargarComunas);
+
+// Cargar las regiones al cargar la página
+cargarComunas();
