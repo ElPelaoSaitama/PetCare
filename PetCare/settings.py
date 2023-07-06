146589835f5fb4f0130671f9f7315b8a215b3b10
Gustavo Cartes <gustavocartes05@gmail.com>
2023-07-06 11:38:56 -0400

    import jazzmin librery

--- PetCare/settings.py
+++ PetCare/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .jazzmin import JAZZMIN_SETTINGS
+
+JAZZMIN_SETTINGS = JAZZMIN_SETTINGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
